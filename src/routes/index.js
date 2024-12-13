90e87dd8275a5927076db76815acffe9318aae46
vung-cntt <levanvung9111@gmail.com>
2024-12-13 15:21:02 +0700

    -update api key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
-'use-strict'
+"use-strict";
+const { apiKey, permission } = require("../auth/checkAuth");
+const express = require("express");
+const router = express.Router();
+//check api key
+router.use(apiKey);
+router.use(permission("0000"));
+router.use("/v1/api", require("./access"));
 
-const express = require('express')
-const router = express.Router()
-router.use('/v1/api', require('./access'))
-
-module.exports = router
+module.exports = router;
