3ffdd62712602e99ee096b91aba9f95ff471126c
levanvung <levanvung113@gmail.com>
2024-12-16 15:49:11 +0700

    handle success reponse

--- src/controller/access.controller.js
+++ src/controller/access.controller.js
@@ -1,8 +1,16 @@
 'use strict';
 const AccessService = require("../services/access.service");
+const {OK, CREATED} = require('../core/success.response')
 class AcessController {
     async signUp(req, res, next) {
-        return res.status(201).json(await AccessService.signUp(req.body));
+      new CREATED({
+        message: "Register successfully",
+        metadata : await AccessService.signUp(req.body),
+        options: {
+          limit: 10
+        }
+      }).send(res)
+        // return res.status(201).json(await AccessService.signUp(req.body));
     }
   }
   
