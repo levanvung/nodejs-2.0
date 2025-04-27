a48c2d5c9b39df1629193bf3b356bf53abf7c223
Lê Văn Vững <levanvung113@gmail.com>
2025-04-27 14:14:34 +0700

    update

--- src/routes/public/index.js
+++ src/routes/public/index.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get("/product/search/:keySearch?", asyncHandler(productController.searchProducts));
 router.get("/product/findOne/:product_id", asyncHandler(productController.findOneProducts));
 router.get("/product/published", asyncHandler(productController.getAllPublishedProducts));
+router.get("/product/hot", asyncHandler(productController.getHotProducts));
 
 module.exports = router; 
